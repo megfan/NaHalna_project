bf1886f0dce99f2f7de2a83008944fb3def8897b
megfan <megfa@yahoo.com>
2018-09-08 21:10:34 +0200

    NaHalna update

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    var controller = new ScrollMagic.Controller();
+
+
+    var pinIntroScene = new ScrollMagic.Scene({
+        triggerElement: '.text-box',
+        // triggerHook: 0,
+        duration: '10%'
+
+    })
+        .setPin('.text-box', {pushFollowers:false})
+        .addTo(controller);
+
+    $('.project').each(function () {
+        var ourScene = new ScrollMagic.Scene({
+
+            triggerElement: this.children[0],
+            duration: '80%',
+            // triggerHook: 0.9,
+            // reverse: false
+        })
+            .setClassToggle(this, 'fade-in')
+            .addIndicators({
+                name: 'fade-scene',
+                colorTrigger: 'pink',
+                colorStart: 'yellow',
+                colorEnd: 'blue'
+            })
+            .addTo(controller);
+    })
+});
