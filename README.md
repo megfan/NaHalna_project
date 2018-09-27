44c65e87103156ed11485f1701475ba2e7093df3
megfan <megfa@yahoo.com>
2018-09-27 10:56:58 +0200

    Readme update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # NaHalna_project
+This is a one page webpage made for NaHalna - graphic designer.
+I created it mainly for learning purposes.
+It uses CSS Grid, Scroll Magic, jQuery and gallery section made in Java SCript.
+I also included Font Awesome and Google Fonts.
+Website is fully responsive.
+
+## View Demo:
+soon :)
+
+## Built With
+
+* CSS Grid
+* jQuery
+* Java SCript
+* Scroll Magic
+* Sass
+
+## Authors
+
+* **Margaret Staszewska**
--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-  entry: './js/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'out.js'
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: { 
-            presets: ['env', 'stage-2', 'react']
-          }
-        }
-      },
-      { 
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|jpg|svg|gif|webp)/,
-        use: 'file-loader'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    })],
-  devServer: {
-    historyApiFallback: true
-  }
+    entry: './js/Main.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'out.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['env', 'stage-2', 'react']
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif|webp|mov|mp4)/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })],
+    devServer: {
+        historyApiFallback: true
+    }
 };
